7691a65db432b8c19a972dfed4c0edb7447f9d6b
Chenguang Huang <130-s0011@users.noreply.gitlab.vision.in.tum.de>
2019-05-05 13:22:15 +0000

    feat(ex1.h): add user_implemented_expmap for SO3

--- include/visnav/ex1.h
+++ include/visnav/ex1.h
@@ -42,7 +42,21 @@ namespace visnav {
 template <class T>
 Eigen::Matrix<T, 3, 3> user_implemented_expmap(
     const Eigen::Matrix<T, 3, 1>& xi) {
-  // TODO SHEET 1: implement
+  // TODO SHEET 1: implementntit
+
+    Eigen::Matrix<T, 3, 1> a = xi;
+    Eigen::Matrix<T, 3, 3> R;
+    Eigen::Matrix<T, 3, 3> A = Eigen::MatrixXd::Zero(3, 4);
+    double theta = a.norm().sqrt();
+    a = a / theta;
+    A(1,0) = a(2,0);
+    A(2,0) = -a(1,0);
+    A(2,1) = a(0,0);
+    A(0,1) = -a(2,0);
+    A(0,2) = a(1,0);
+    A(1,2) = -a(0,0);
+    R = Eigen::MatrixXd::Identity(3,3)*cos(theta)+(1-cos(theta))*a*a.transpose()+sin(theta)*A;
+    return R;
 }
 
 // Implement log for SO(3)
