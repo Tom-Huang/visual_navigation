ac4ff96280c2f64513969bebc6ae90913f620843
Andong Tan <ge68kiy@tum.de>
2019-07-21 16:31:38 +0200

    updated PPt

--- python_script/plot_compare_graph.py
+++ python_script/plot_compare_graph.py
@@ -12,6 +12,8 @@ z = [430.423,400.085,333.723,305.043,307.711,301.82,274.728,268.572,291.259,242.
 
 # detection time
 w = [3.03643,2.74359,2.73232,2.43286,2.26378,2.62328,2.52254,2.26326,2.7443,2.42524,2.23357,2.42336]
+x_ex5 = [0.35,0.5]
+w_ex5 = [44.0214,44.0214]
 
 # f2f optical flow time
 k = [143.192,128.286,133.53,117.438,120.597,135.881,127.484,120.76,128.453,118.085,118.31,133.065]
@@ -32,6 +34,7 @@ plt.ylabel('optimization time/second')
 fig3 = plt.figure(3)
 fig3.suptitle("empty cells percentage vs detection time")
 plt.plot(x,w,"bo-")
+plt.plot(x_ex5,w_ex5,"r-")
 plt.xlabel('empty cells percentage')
 plt.ylabel('detection time/second')
 #plt.ylim(0.10,0.16)
