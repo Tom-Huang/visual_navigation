a04dd05ee782ce4bd1611307792ddfa1450a76a0
Andong Tan <ge68kiy@tum.de>
2019-07-21 13:25:31 +0200

    parameter tuning

--- src/odometry.cpp
+++ src/odometry.cpp
@@ -96,7 +96,7 @@ constexpr int NUM_CAMS = 2;
 int rnum = 15;
 int cnum = 23;
 int threshold = 100;  // threshold for minimum num of points
-double empty_cells_percentage_threshold = 0.43;
+double empty_cells_percentage_threshold = 0.45;
 int threshold2 = int(
     rnum * cnum * empty_cells_percentage_threshold);  //  threshold for maximum
                                                       //  num of empty cells
